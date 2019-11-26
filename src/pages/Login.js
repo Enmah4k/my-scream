f649b8143aad62c230a81d53ee2840171e96defa
Enmah4k <enmah4k@gmail.com>
2019-11-26 13:18:27 -0400

    Login start

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import whithStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+import AppIcon from '../images/icon.png'
+import Typography from '@material-ui/core/Typography'
 
-export class Login extends Component {
+const styles = {
+    form: {
+        textAling: 'center'
+    },
+
+    pageTitle: {
+        margin: 'auto'
+    }
+}
+
+class Login extends Component {
     render() {
+        const { classes } = this.props
         return (
-            <div>
-                <h1>Login Page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm></Grid>
+                <Grid item sm>
+                    <img src={AppIcon} alt="icon"></img>
+                    <Typography variant="h1" className={classes.pageTitle}>
+                        Login 
+                    </Typography>
+                </Grid>
+                <Grid item sm></Grid>
+            </Grid>
         )
     }
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default whithStyles(styles)(Login);
